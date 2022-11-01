e4175dd292250deebcd1998b3e4f6c005cd989d6
Koboo <admin@koboo.eu>
2022-11-01 11:15:20 +0100

    Fix exception message

--- src/main/java/eu/koboo/en2do/repository/exception/RepositoryIdNotFoundException.java
+++ src/main/java/eu/koboo/en2do/repository/exception/RepositoryIdNotFoundException.java
@@ -5,6 +5,6 @@ import eu.koboo.en2do.repository.annotation.Id;
 public class RepositoryIdNotFoundException extends Exception {
 
     public RepositoryIdNotFoundException(Class<?> repoClass) {
-        super("Couldn't the " + Id.class + " field in entity of" + repoClass.getName() + "! That's a required annotation.");
+        super("Couldn't find " + Id.class.getName() + " field in entity of " + repoClass.getName() + "! That's a required annotation.");
     }
 }
