af082d68188e377dabaff91830ef25aa2cd47b54
Koboo <14143908+Koboo@users.noreply.github.com>
2022-11-11 13:01:37 +0100

    Add boolean "not" to FilterType to check if a filter should be negotiated

--- src/main/java/eu/koboo/en2do/methods/FilterType.java
+++ src/main/java/eu/koboo/en2do/methods/FilterType.java
@@ -2,5 +2,5 @@ package eu.koboo.en2do.methods;
 
 import java.lang.reflect.Field;
 
-public record FilterType(Field field, FilterOperator operator) {
+public record FilterType(Field field, boolean notFilter, FilterOperator operator) {
 }
