f550de2693b4d79ed82a5362883f5cc038342823
Koboo <14143908+Koboo@users.noreply.github.com>
2022-11-11 13:06:41 +0100

    Remove unused exceptions

--- src/main/java/eu/koboo/en2do/exception/MethodUnsupportedReturnTypeException.java
+++
@@ -1,9 +0,0 @@
-package eu.koboo.en2do.exception;
-
-public class MethodUnsupportedReturnTypeException extends Exception {
-
-    public MethodUnsupportedReturnTypeException(Class<?> returnTypeClass, String methodName, Class<?> repoClass) {
-        super("Unsupported return-type " + returnTypeClass.getName() + " at method " +
-                methodName + " in " + repoClass.getName());
-    }
-}
