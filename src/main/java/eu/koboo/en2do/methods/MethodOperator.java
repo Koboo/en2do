b38d2516de02635e19d21ef3e75921f006c2154e
Koboo <14143908+Koboo@users.noreply.github.com>
2022-11-11 10:52:35 +0100

    Add Validators to MethodOperator

--- src/main/java/eu/koboo/en2do/methods/MethodOperator.java
+++ src/main/java/eu/koboo/en2do/methods/MethodOperator.java
@@ -1,23 +1,53 @@
 package eu.koboo.en2do.methods;
 
+import eu.koboo.en2do.exception.*;
+import eu.koboo.en2do.utility.GenericUtils;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
+
+import java.util.List;
 
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Getter
 public enum MethodOperator {
 
-    FIND("findBy"),
-    DELETE("deleteBy"),
-    EXISTS("existsBy"),
-    COUNT("countBy");
+    FIND_FIRST("findFirstBy", (method, returnType, entityClass, repoClass) -> {
+        if(!GenericUtils.isTypeOf(entityClass, returnType)) {
+            throw new MethodFindReturnTypeException(method, entityClass, repoClass);
+        }
+    }),
+    FIND_MANY("findManyBy", (method, returnType, entityClass, repoClass) -> {
+        if(!GenericUtils.isTypeOf(List.class, entityClass)) {
+            throw new MethodFindListReturnTypeException(method, entityClass, repoClass);
+        }
+        Class<?> listType = GenericUtils.getGenericTypeOfReturnList(method);
+        if (!listType.isAssignableFrom(entityClass)) {
+            throw new MethodFindListTypeException(method, repoClass, listType);
+        }
+    }),
+    DELETE("deleteBy", (method, returnType, entityClass, repoClass) -> {
+        if (!GenericUtils.isTypeOf(Boolean.class, returnType)) {
+            throw new MethodBooleanReturnTypeException(method, repoClass);
+        }
+    }),
+    EXISTS("existsBy", (method, returnType, entityClass, repoClass) -> {
+        if (!GenericUtils.isTypeOf(Boolean.class, returnType)) {
+            throw new MethodBooleanReturnTypeException(method, repoClass);
+        }
+    }),
+    COUNT("countBy", ((method, returnType, entityClass, repoClass) -> {
+        if (!GenericUtils.isTypeOf(Long.class, returnType)) {
+            throw new MethodLongReturnTypeException(method, repoClass);
+        }
+    }));
 
     public static final MethodOperator[] VALUES = MethodOperator.values();
 
     String keyword;
+    ReturnTypeValidator returnTypeValidator;
 
     public String removeOperatorFrom(String textWithOperator) {
         return textWithOperator.replaceFirst(getKeyword(), "");
