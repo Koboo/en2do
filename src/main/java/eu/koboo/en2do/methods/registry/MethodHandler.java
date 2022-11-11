64837fa1228b5eda342c88352e8284b4834a09c3
Koboo <14143908+Koboo@users.noreply.github.com>
2022-11-11 13:08:33 +0100

    Remove generic parameter type of MethodHandler

--- src/main/java/eu/koboo/en2do/methods/registry/MethodHandler.java
+++ src/main/java/eu/koboo/en2do/methods/registry/MethodHandler.java
@@ -2,7 +2,7 @@ package eu.koboo.en2do.methods.registry;
 
 import java.lang.reflect.Method;
 
-public interface MethodHandler<E> {
+public interface MethodHandler {
 
     Object handle(Method method, Object[] arguments) throws Exception;
 }
