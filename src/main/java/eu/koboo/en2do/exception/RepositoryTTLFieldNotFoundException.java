5fab46140870f79e6ccf9133f3b2135975240d87
Koboo <14143908+Koboo@users.noreply.github.com>
2022-12-30 11:18:51 +0100

    Create exception for ttl field validation

---
+++ src/main/java/eu/koboo/en2do/exception/RepositoryTTLFieldNotFoundException.java
@@ -0,0 +1,11 @@
+package eu.koboo.en2do.exception;
+
+import java.util.Date;
+
+public class RepositoryTTLFieldNotFoundException extends Exception {
+
+    public RepositoryTTLFieldNotFoundException(Class<?> repoClass, String indexName) {
+        super("Couldn't find " + indexName + " field for time-to-live index in entity of " + repoClass.getName() + ". " +
+                "To set a ttl index the field has to be type " + Date.class.getName() + ".");
+    }
+}
