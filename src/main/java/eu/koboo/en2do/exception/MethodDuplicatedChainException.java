2b739ca77fd44b006a1a6da56b136e2cd84fcdad
Koboo <14143908+Koboo@users.noreply.github.com>
2023-01-04 14:37:07 +0100

    Update exception message

--- src/main/java/eu/koboo/en2do/exception/MethodDuplicatedChainException.java
+++ src/main/java/eu/koboo/en2do/exception/MethodDuplicatedChainException.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Method;
 public class MethodDuplicatedChainException extends Exception {
 
     public MethodDuplicatedChainException(Method method, Class<?> entityClass) {
-        super("Can't use \"and\" and \"or\"  operation in one method, but exists in " +
+        super("Can't use \"And\" + \"Or\"  operation in one method, but exists in " +
                 method.getName() + " of " + entityClass.getName());
     }
 }
