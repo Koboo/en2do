33af3ec0a24bd1c578be01e9e57d4a4a8f22f1a6
Koboo <14143908+Koboo@users.noreply.github.com>
2023-01-04 14:37:35 +0100

    Create abstract class to extend default repository method

---
+++ src/main/java/eu/koboo/en2do/repository/RepositoryMethod.java
@@ -0,0 +1,17 @@
+package eu.koboo.en2do.repository;
+
+import com.mongodb.client.MongoCollection;
+import eu.koboo.en2do.Repository;
+import eu.koboo.en2do.meta.RepositoryMeta;
+import eu.koboo.en2do.meta.registry.MethodHandler;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PROTECTED, makeFinal = true)
+@RequiredArgsConstructor
+public abstract class RepositoryMethod<E, ID, R extends Repository<E, ID>> implements MethodHandler {
+
+    RepositoryMeta<E, ID, R> repositoryMeta;
+    MongoCollection<E> entityCollection;
+}
