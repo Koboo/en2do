bd2c47d884f98f17a07d7f8cd3e1c5cf14118fef
Koboo <14143908+Koboo@users.noreply.github.com>
2023-01-18 15:13:59 +0100

    Create new + Update old exceptions

---
+++ src/main/java/eu/koboo/en2do/exception/MethodPageNotAllowedException.java
@@ -0,0 +1,11 @@
+package eu.koboo.en2do.exception;
+
+import java.lang.reflect.Method;
+
+public class MethodPageNotAllowedException extends Exception {
+
+    public MethodPageNotAllowedException(Method method, Class<?> repoClass) {
+        super("The method " + method.getName() + " in " +
+                repoClass.getName() + " is not allowed for pagination.");
+    }
+}
