46a121f418c1cfbc6aeef56d854815c23d457e7d
Koboo <14143908+Koboo@users.noreply.github.com>
2023-01-18 13:26:29 +0100

    Create SortAll test-unit

---
+++ src/test/java/eu/koboo/en2do/test/customer/tests/CustomerSortAllTest.java
@@ -0,0 +1,57 @@
+package eu.koboo.en2do.test.customer.tests;
+
+import eu.koboo.en2do.repository.options.methods.sort.Sort;
+import eu.koboo.en2do.test.Const;
+import eu.koboo.en2do.test.customer.Customer;
+import eu.koboo.en2do.test.customer.CustomerRepositoryTest;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomerSortAllTest extends CustomerRepositoryTest {
+
+    @Test
+    @Order(1)
+    public void cleanUpRepository() {
+        List<Customer> customerList = repository.findAll();
+        assertNotNull(customerList);
+        assertTrue(customerList.isEmpty());
+    }
+
+    @Test
+    @Order(2)
+    public void saveCustomer() {
+        for (int i = 0; i < 15; i++) {
+            Customer customer = Const.createNewCustomer();
+            customer.setUniqueId(UUID.randomUUID());
+            customer.setCustomerId(i);
+            assertTrue(repository.save(customer));
+            assertTrue(repository.exists(customer));
+        }
+    }
+
+    @Test
+    @Order(3)
+    public void findCustomer() {
+        List<Customer> customerList = repository.sortAll(Sort.of()
+                .order("customerId", true)
+                .limit(10)
+                .skip(5)
+        );
+        assertNotNull(customerList);
+        assertFalse(customerList.isEmpty());
+        assertEquals(10, customerList.size());
+        for (Customer customer : customerList) {
+            assertNotNull(customer);
+            assertEquals(Const.FIRST_NAME, customer.getFirstName());
+            assertEquals(Const.LAST_NAME, customer.getLastName());
+            assertEquals(Const.BIRTHDAY, customer.getBirthday());
+            assertEquals(Const.PHONE_NUMBER, customer.getPhoneNumber());
+            assertEquals(Const.ORDERS.size(), customer.getOrders().size());
+        }
+    }
+}
