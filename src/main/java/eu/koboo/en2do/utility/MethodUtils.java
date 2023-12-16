e821a0e82c8681641f6a641e340e8a5c54e70563
Koboo <14143908+Koboo@users.noreply.github.com>
2023-12-16 14:45:15 +0100

    Add comment to utils object

--- src/main/java/eu/koboo/en2do/utility/MethodUtils.java
+++ src/main/java/eu/koboo/en2do/utility/MethodUtils.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class MethodUtils {
 
+    /**
+     * These methods are ignored from en2do validation, because they are default java
+     * methods, which doesn't get implement by en2do repositories.
+     */
     public static final List<String> IGNORED_DEFAULT_METHODS = Arrays.asList(
         "notify", "notifyAll", "wait", "finalize", "clone"
     );
