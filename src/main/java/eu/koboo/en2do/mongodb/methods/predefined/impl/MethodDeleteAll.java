b69ec78192d3af26963979de2b65fc601bb44c89
Koboo <14143908+Koboo@users.noreply.github.com>
2023-12-16 14:34:44 +0100

    Optimize deleteAll predefined Method

--- src/main/java/eu/koboo/en2do/mongodb/methods/predefined/impl/MethodDeleteAll.java
+++ src/main/java/eu/koboo/en2do/mongodb/methods/predefined/impl/MethodDeleteAll.java
@@ -1,12 +1,14 @@
 package eu.koboo.en2do.mongodb.methods.predefined.impl;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import eu.koboo.en2do.mongodb.RepositoryMeta;
 import eu.koboo.en2do.mongodb.methods.predefined.PredefinedMethod;
 import eu.koboo.en2do.repository.Repository;
 import org.bson.conversions.Bson;
 
 import java.lang.reflect.Method;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MethodDeleteAll<E, ID, R extends Repository<E, ID>> extends PredefinedMethod<E, ID, R> {
@@ -19,11 +21,16 @@ public class MethodDeleteAll<E, ID, R extends Repository<E, ID>> extends Predefi
     public Object handle(Method method, Object[] arguments) throws Exception {
         // TODO: Check for Id in check instead of deleting every single entity itself.
         List<E> entityList = repositoryMeta.checkEntityList(method, arguments[0]);
+        if(entityList.isEmpty()) {
+            return true;
+        }
+        List<ID> idList = new LinkedList<>();
         for (E entity : entityList) {
             ID uniqueId = repositoryMeta.checkUniqueId(method, repositoryMeta.getUniqueId(entity));
-            Bson idFilter = repositoryMeta.createIdFilter(uniqueId);
-            entityCollection.deleteOne(idFilter);
+            idList.add(uniqueId);
         }
+        Bson idFilter = Filters.in("_id", idList);
+        entityCollection.deleteMany(idFilter);
         return true;
     }
 }
