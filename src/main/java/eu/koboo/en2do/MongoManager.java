29f90621e38e25869d70e25ba826d792beb76e1c
Koboo <14143908+Koboo@users.noreply.github.com>
2023-12-21 22:14:13 +0100

    Add constructor with only SettingsBuilder

--- src/main/java/eu/koboo/en2do/MongoManager.java
+++ src/main/java/eu/koboo/en2do/MongoManager.java
@@ -155,6 +155,10 @@ public class MongoManager {
         this(credentials, null, builder);
     }
 
+    public MongoManager(SettingsBuilder builder) {
+        this(null, null, builder);
+    }
+
     public MongoManager(Credentials credentials) {
         this(credentials, null, null);
     }
