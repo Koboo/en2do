3c8c61b9280f984c2841f0cf5011133cc9898133
Koboo <14143908+Koboo@users.noreply.github.com>
2024-02-29 19:26:50 +0100

    Allow Transient annotation on methods

--- src/main/java/eu/koboo/en2do/mongodb/methods/predefined/impl/MethodDeleteAll.java
+++ src/main/java/eu/koboo/en2do/mongodb/methods/predefined/impl/MethodDeleteAll.java
@@ -1,16 +1,11 @@
 package eu.koboo.en2do.mongodb.methods.predefined.impl;
 
-import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.DeleteResult;
 import eu.koboo.en2do.mongodb.RepositoryMeta;
 import eu.koboo.en2do.mongodb.methods.predefined.GlobalPredefinedMethod;
 import eu.koboo.en2do.repository.Repository;
-import org.bson.conversions.Bson;
 
 import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 
 public class MethodDeleteAll extends GlobalPredefinedMethod {
 
--- src/main/java/eu/koboo/en2do/repository/entity/Transient.java
+++ src/main/java/eu/koboo/en2do/repository/entity/Transient.java
@@ -9,6 +9,6 @@ import java.lang.annotation.*;
  */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target({ElementType.FIELD,ElementType.METHOD})
 public @interface Transient {
 }
