c27e629820acb102b8d8ff7990842c79de5ecc21
Koboo <14143908+Koboo@users.noreply.github.com>
2024-10-13 14:16:06 +0200

    Add static constructor for Order test object

--- src/test/java/eu/koboo/en2do/test/customer/Order.java
+++ src/test/java/eu/koboo/en2do/test/customer/Order.java
@@ -3,7 +3,9 @@ package eu.koboo.en2do.test.customer;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Getter // lombok
 @Setter // lombok
@@ -13,7 +15,21 @@ import java.util.List;
 @ToString // lombok
 public class Order {
 
+    private static final Random RANDOM = new Random();
+
     String orderText;
     double orderPrice;
     List<Integer> positionIds;
+
+    public static Order create(double price, String text) {
+        return new Order() {{
+            setOrderPrice(price);
+            setOrderText(text);
+            List<Integer> positionIds = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                positionIds.add(RANDOM.nextInt());
+            }
+            setPositionIds(positionIds);
+        }};
+    }
 }
