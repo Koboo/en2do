62102218d119d9bfb4f2e1c2d93ab4a3d1e89158
Koboo <14143908+Koboo@users.noreply.github.com>
2024-10-29 09:25:58 +0100

    Delete duplicated unit tests

--- src/test/java/eu/koboo/en2do/test/alien/Planet.java
+++
@@ -1,6 +0,0 @@
-package eu.koboo.en2do.test.alien;
-
-public enum Planet {
-
-    EARTH, MARS, NEPTUNE, SATURN
-}
--- src/test/java/eu/koboo/en2do/test/mapkeys/MapKeyTest.java
+++ src/test/java/eu/koboo/en2do/test/mapkeys/MapKeyTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.bson.assertions.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MapKeyTest extends RepositoryTest<MapKey, UUID, MapKeyRepository> {
@@ -31,16 +32,27 @@ public class MapKeyTest extends RepositoryTest<MapKey, UUID, MapKeyRepository> {
 
         mapKey = repository.findFirstById(uniqueId);
         assertNotNull(mapKey);
+
         assertNotNull(mapKey.getStringMap());
+        assertEquals(1, mapKey.getStringMap().size());
         assertTrue(mapKey.getStringMap().containsKey("Test"));
+
         assertNotNull(mapKey.getBooleanMap());
+        assertEquals(1, mapKey.getBooleanMap().size());
         assertTrue(mapKey.getBooleanMap().containsKey(true));
+
         assertNotNull(mapKey.getDoubleMap());
+        assertEquals(1, mapKey.getDoubleMap().size());
         assertTrue(mapKey.getDoubleMap().containsKey(1.234));
+
         assertNotNull(mapKey.getUuidMap());
+        assertEquals(1, mapKey.getUuidMap().size());
         assertTrue(mapKey.getUuidMap().containsKey(uuidKey));
+
         assertNotNull(mapKey.getEnumMap());
+        assertEquals(1, mapKey.getEnumMap().size());
         assertTrue(mapKey.getEnumMap().containsKey(MapKeyEnum.ONE));
+
         assertTrue(repository.drop());
     }
 }
