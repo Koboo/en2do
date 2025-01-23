28b3706f5a8adf92ec50e4aa176f9e84a09bcafa
Koboo <14143908+Koboo@users.noreply.github.com>
2025-01-23 10:52:18 +0100

    Create PrimitiveUtils + unit-test

---
+++ src/main/java/eu/koboo/en2do/utility/PrimitiveUtils.java
@@ -0,0 +1,65 @@
+package eu.koboo.en2do.utility;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PrimitiveUtils {
+
+    public Class<?> wrapperOf(Class<?> primitiveClass) {
+        if (primitiveClass.equals(int.class)) {
+            return Integer.class;
+        }
+        if (primitiveClass.equals(int[].class)) {
+            return Integer[].class;
+        }
+        if (primitiveClass.equals(long.class)) {
+            return Long.class;
+        }
+        if (primitiveClass.equals(long[].class)) {
+            return Long[].class;
+        }
+        if (primitiveClass.equals(boolean.class)) {
+            return Boolean.class;
+        }
+        if (primitiveClass.equals(boolean[].class)) {
+            return Boolean[].class;
+        }
+        if (primitiveClass.equals(byte.class)) {
+            return Byte.class;
+        }
+        if (primitiveClass.equals(byte[].class)) {
+            return Byte[].class;
+        }
+        if (primitiveClass.equals(char.class)) {
+            return Character.class;
+        }
+        if (primitiveClass.equals(char[].class)) {
+            return Character[].class;
+        }
+        if (primitiveClass.equals(float.class)) {
+            return Float.class;
+        }
+        if (primitiveClass.equals(float[].class)) {
+            return Float[].class;
+        }
+        if (primitiveClass.equals(double.class)) {
+            return Double.class;
+        }
+        if (primitiveClass.equals(double[].class)) {
+            return Double[].class;
+        }
+        if (primitiveClass.equals(short.class)) {
+            return Short.class;
+        }
+        if (primitiveClass.equals(short[].class)) {
+            return Short[].class;
+        }
+        if (primitiveClass.equals(void.class)) {
+            return Void.class;
+        }
+        if(!primitiveClass.isPrimitive()) {
+            return primitiveClass;
+        }
+        return null;
+    }
+}
