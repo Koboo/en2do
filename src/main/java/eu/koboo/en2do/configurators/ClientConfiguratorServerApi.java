0333ed651f2a156f42014888c166e94e659eb041
Koboo <14143908+Koboo@users.noreply.github.com>
2025-03-30 12:26:46 +0200

    Add default client configurator for server api

---
+++ src/main/java/eu/koboo/en2do/configurators/ClientConfiguratorServerApi.java
@@ -0,0 +1,24 @@
+package eu.koboo.en2do.configurators;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import eu.koboo.en2do.ClientConfigurator;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@RequiredArgsConstructor
+@SuppressWarnings("unused")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public final class ClientConfiguratorServerApi implements ClientConfigurator {
+
+    ServerApi serverApi;
+
+    @Override
+    public void configure(MongoClientSettings.Builder settingsBuilder) {
+        if(serverApi == null) {
+            throw new NullPointerException("serverApi is null");
+        }
+        settingsBuilder.serverApi(serverApi);
+    }
+}
